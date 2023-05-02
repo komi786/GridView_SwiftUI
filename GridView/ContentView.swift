5421886f93a154d9b7e66fac63c01f301c154c0a
Komal Gilani <komalsyeda29@gmail.com>
2023-05-02 22:58:50 +0500

    Update ContentView.swift
    first commit

--- GridView/ContentView.swift
+++ GridView/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var data : [Int] =  Array(1...15)
+    private var colors : [Color] = [.red ,.green ,.cyan ,.purple]
+    
+    private let adaptiveColumn = [
+        GridItem(.adaptive(minimum: 150))  //it adapts to screen size and adjust columns according to screen width
+        
+    ]
+    private let numberColumn = [
+        GridItem(.flexible()), GridItem(.flexible())
+    ]
+    private let fixedColumn = [
+    
+        GridItem(.fixed(200)),
+        GridItem(.fixed(200))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        NavigationView{
+            ScrollView{
+                LazyVGrid(columns: fixedColumn,spacing: 20){
+                    
+                    ForEach(data, id:\.self){ number in
+                        ZStack{
+                            Rectangle()
+                                .frame(width: 150,height: 150)
+                                .foregroundColor(colors[number%4])
+                            Text("\(number)")
+                                .font(.system(size: 80,weight: .medium,design: .rounded))
+                                .foregroundColor(.white)
+                        }
+                        
+                    } // end of For Each loop
+                } //end of LazyVGrid
+                
+            }  //end of ScrollView
+            .navigationTitle("Simple Grid View")
+        } //end of Navigation View
         .padding()
+       
     }
 }
 
