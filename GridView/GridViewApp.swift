86dcd28302fe37a7036bf848816228b017086a96
Komal Gilani <komalsyeda29@gmail.com>
2023-05-02 15:17:44 +0500

    Initial Commit

---
+++ GridView/GridViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridViewApp.swift
+//  GridView
+//
+//  Created by Komal Gilani on 02/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct GridViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
